932b8c6a1d86aa090c7d9662a6255d6e11bbc5e4
Michael Merid <michaelbelay818@gmail.comgit add first.sgit add arithmetic1.sgit commit -m New>
2020-02-10 08:36:51 -0500

    Arithmetic1Program

---
+++ arithmetic1.s
@@ -0,0 +1,16 @@
+@arithmetic1 program
+.section .data
+.section .text
+.global _start
+_start:
+    mov	   r1,#10	@ load r1 with 10
+    mov    r2,#11	@ load r2 with 11
+    mov	   r3,#7	@ load r3 with 7
+    mov    r4,#2	@ load r4 with 2
+    add    r1,r1,r2	@ add r2 to r1 and store in r1
+    mul    r3,r3,r4	@ multiply r4 to r3 and store in r3
+    sub    r1,r1,r3	@ subtract r3 from r1 and store in r1
+
+    mov r7,#1		@ Program Termination:exit syscall
+    svc #0		@ Program Termination:wake kernel
+.end
